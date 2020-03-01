a7f7b314e4256bfb1626553f291ee4aeb2bb1805
Sumith YesuDasan <ydsumith@users.noreply.github.com>
2020-02-29 19:27:38 -0600

    Update 2_IP_cam_python.py

--- Sumith/2_IP_cam_python.py
+++ Sumith/2_IP_cam_python.py
@@ -1,16 +1,16 @@
 import cv2
 
-#print("Before URL")
-cap = cv2.VideoCapture('rtsp://admin:123456@192.168.1.216/H264?ch=1&subtype=0')
-#print("After URL")
+print("Before URL")
+cap = cv2.VideoCapture('rtsp://admin:admin@192.168.1.4:8080/h264_ulaw.sdp')
+print("After URL")
 
 while True:
 
-    #print('About to start the Read command')
+    print('About to start the Read command')
     ret, frame = cap.read()
-    #print('About to show frame of Video.')
+    print('About to show frame of Video.')
     cv2.imshow("Capturing",frame)
-    #print('Running..')
+    print('Running..')
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
