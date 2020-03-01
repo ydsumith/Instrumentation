15c2bfb6c3d0995f6fd65b0e5efc444b0aed2234
Sumith YesuDasan <ydsumith@users.noreply.github.com>
2020-02-29 20:57:29 -0600

    Update 2_IP_cam_python.py

--- Sumith/2_IP_cam_python.py
+++ Sumith/2_IP_cam_python.py
@@ -1,17 +1,29 @@
+# Written by Sumith Yesudasan
+# Last modified FEB 29 2020
+
 import cv2
 
 print("Before URL")
 cap = cv2.VideoCapture('rtsp://admin:admin@192.168.1.4:8080/h264_ulaw.sdp')
-print("After URL")
+print("running")
 
-while True:
+increment = 1;
 
-    print('About to start the Read command')
-    ret, frame = cap.read()
-    print('About to show frame of Video.')
-    cv2.imshow("Capturing",frame)
-    print('Running..')
+def rescale_frame(frame, percent):
+    width = int(frame.shape[1] * percent/ 100)
+    height = int(frame.shape[0] * percent/ 100)
+    dim = (width, height)
+    return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
+
+while increment < 1000:
+    ret, framer = cap.read()
     
+    frame = rescale_frame(framer, percent=50)
+    
+    cv2.imshow("Capturing",frame)
+    
+    increment = increment +1;
+
     #Need to work on the stopping criteria
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
